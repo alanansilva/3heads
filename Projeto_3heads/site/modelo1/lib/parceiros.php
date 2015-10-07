3780c4345551abc659a82dd91f9f884a44a6c45e
Alana Nunes <alanansilva@gmail.com>
2015-10-07 13:48:59 -0300

    modificações nomes arquivos2

--- Projeto_3heads/site/modelo1/lib/parceiros.php
+++
@@ -1,6 +0,0 @@
-<!-- banner-slider -->
-<div id="parceiros" class="parceiros">
-    <h1>Parceiros</h1>
-</div>
-<div class="clearfix"></div>
-<!-- //banner-slider -->
--- Projeto_3heads/site/modelo1/lib/produtos.php
+++
@@ -1,6 +0,0 @@
-<!-- banner-slider -->
-<div id="produtos" class="produtos">
-    <h1>Produtos</h1>
-</div>
-<div class="clearfix"></div>
-<!-- //banner-slider -->
